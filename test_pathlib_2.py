e05223073644b5a0a1ec894ff93fbc088750aff9
imayuki-prog <nyorohas@gmail.com>
2024-03-07 11:30:58 -0800

    update-win

--- test_pathlib_2.py
+++ test_pathlib_2.py
@@ -11,5 +11,5 @@ for i in p.rglob('*.*'):
 columns = ["File_name", "Parent", "Created"]
 df = pd.DataFrame.from_records(all_files, columns=columns)
 df = df[~df["File_name"].str.contains(".csv") == False]
-df = df[df["Parent"].str.contains("2024-sales", regex=False) == False]
+df = df[df["Parent"].astype(str).str.contains("2024-sales-test")] # "Parent" is "Path" object therefore it needs to convert to "Strings"
 print(df)
